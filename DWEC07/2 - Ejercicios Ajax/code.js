5173c048cd48408df9b6ab838f5024dbab97b5ef
Brais <braiscao.dev@gmail.com>
2019-05-22 21:31:40 +0200

    Ejercicio 7 vanilla

--- DWEC07/2 - Ejercicios Ajax/code.js
+++ DWEC07/2 - Ejercicios Ajax/code.js
@@ -1,4 +1,6 @@
-$(window).on("load", function() {
+/* VERSION JQUERY*/
+
+/* $(window).on("load", function() {
     $('input[type="button"]').click(comprobarNombre);
 
     function comprobarNombre() {
@@ -20,4 +22,28 @@ $(window).on("load", function() {
             }
         });
     }
-});
+}); */
+
+/* VERSION VANILLA JS*/
+
+window.onload = function() {
+    document.querySelector('input[type="button"]').addEventListener("click", comprobarNombre);
+
+    function comprobarNombre() {
+        var login = document.querySelector("#login").value;
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if (this.response === "si") {
+                    document.querySelector("#disponibilidad").innerHTML = "El nombre de usuario est&aacute; disponible";
+                } else if (this.response === "no") {
+                    document.querySelector("#disponibilidad").innerHTML =
+                        "El nombre de usuario NO est&aacute; disponible";
+                }
+            }
+        };
+
+        xhr.open("GET", "compruebaDisponibilidad.php", true);
+        xhr.send();
+    }
+};
